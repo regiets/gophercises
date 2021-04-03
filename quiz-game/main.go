4a7f32244aa196d020a5c4c96528182193edc676
Philipp Steiger <philippsteiger01@gmail.com>
2021-04-03 13:47:38 +0100

    Print problems to cli

--- quiz-game/main.go
+++ quiz-game/main.go
@@ -20,7 +20,26 @@ func main() {
 	if err != nil {
 		exit("Failed to parse the provided CSV file.")
 	}
-	fmt.Println(lines)
+	problems := parseLines(lines)
+	for i, p := range problems {
+		fmt.Printf("Problem #%d: %s = \n", i+1, p.q)
+	}
+}
+
+func parseLines(lines [][]string) []problem {
+	ret := make([]problem, len(lines))
+	for i, line := range lines {
+		ret[i] = problem{
+			q: line[0],
+			a: line[1],
+		}
+	}
+	return ret
+}
+
+type problem struct {
+	q string
+	a string
 }
 
 func exit(msg string) {
