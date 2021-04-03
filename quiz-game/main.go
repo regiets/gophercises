cbe9bbf467a6f8113f7a2c788f6cea70f347ae35
Philipp Steiger <philippsteiger01@gmail.com>
2021-04-03 14:24:21 +0100

    Improve timer logic to stop and return resultt

--- quiz-game/main.go
+++ quiz-game/main.go
@@ -26,17 +26,22 @@ func main() {
 	problems := parseLines(lines)
 
 	timer := time.NewTimer(time.Duration(*timeLimit) * time.Second)
-
 	correct := 0
+
 	for i, p := range problems {
-		select {
-		case <-timer.C:
-			fmt.Printf("You scored %d out of %d.\n", correct, len(problems))
-			return
-		default:
-			fmt.Printf("Problem #%d: %s = \n", i+1, p.q)
+		fmt.Printf("Problem #%d: %s = ", i+1, p.q)
+		answerCh := make(chan string)
+		go func() {
 			var answer string
 			fmt.Scanf("%s\n", &answer)
+			answerCh <- answer
+		}()
+
+		select {
+		case <-timer.C:
+			fmt.Printf("\nYou scored %d out of %d.\n", correct, len(problems))
+			return
+		case answer := <-answerCh:
 			if answer == p.a {
 				correct++
 			}
